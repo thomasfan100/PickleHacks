090a97015c050b6abaf375aa0a7eda954ca17035
Henri Evjen <henrievjen@Henris-MacBook-Pro.local>
2021-10-29 23:19:39 -0500

    added game summary info modal

--- picklehacks/src/navbar/index.tsx
+++ picklehacks/src/navbar/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './index.css';
 
+import basketball from './images/basketball.png';
+
 function Navbar() {
   return (
-    <div>
-      Navbar
+    <div className="navbar">
+      <img src={basketball} style={{marginLeft: 10, marginTop: 10, height: 50}} />
     </div>
   );
 }
--- picklehacks/src/schedule/index.tsx
+++ picklehacks/src/schedule/index.tsx
@@ -3,14 +3,34 @@
 import React, {useEffect, useState} from 'react';
 import './index.css';
 
-import { Card, DatePicker } from 'antd';
+import { Card, DatePicker, Modal, Input } from 'antd';
+import moment from 'moment';
 import "antd/dist/antd.css";
+
+const { Search } = Input;
 
 function Schedule() {
     let [games, setGames] = useState([]);
-    let [day, setDay] = useState(new Date().getDate());
+    let [day, setDay] = useState(new Date().getDate() - 1);
     let [month, setMonth] = useState(new Date().getMonth());
     let t_games = [];
+
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+    let [currentGame, setCurrentGame] = useState([]);
+
+    let messages = [
+        {
+            "id": 13123,
+            "time": "Thu Oct 28 2021",
+            "message": "Great game!"
+        },
+        {
+            "id": 543543,
+            "time": "Thu Oct 30 2021",
+            "message": "How could they lose! The ref blew the game!!!"
+        }
+    ]
 
     const gridStyle = {
         width: '25%',
@@ -63,16 +83,21 @@ function Schedule() {
 
     return (
         <div>
-            <DatePicker onChange={(date) => {
-                setDay(date?.toDate().getDate());
-                setMonth(date?.toDate().getMonth())
-            }} />
+            <div style={{width: '100%', textAlign: 'center', marginTop: 50, marginBottom: 50}}>
+                <DatePicker defaultValue={moment(new Date(), 'YYYY/MM/DD')} onChange={(date) => {
+                    setDay(date?.toDate().getDate() - 1);
+                    setMonth(date?.toDate().getMonth())
+                }} />
+            </div>
 
             <br />
 
             {games.map((game) => {
                 return (
-                    <Card.Grid style={gridStyle, {display: ((new Date(game[0]).getDate() == day) && (new Date(game[0]).getMonth() == month)) ? "block" : "none"}}>
+                    <Card.Grid style={gridStyle, {display: ((new Date(game[0]).getDate() == day) && (new Date(game[0]).getMonth() == month)) ? "block" : "none"}} onClick={() => {
+                        setCurrentGame(game)
+                        setIsModalVisible(true);
+                    }}>
                         <div>
                             {game[1]} <strong>{game[2]}</strong>
                         </div>
@@ -82,6 +107,36 @@ function Schedule() {
                     </Card.Grid>
                 );
             })}
+
+        <Modal title="Game Summary" visible={isModalVisible} footer={null} onCancel={() => {
+            setIsModalVisible(false);
+        }}>
+            <p>Date: {new Date(currentGame[0]).toDateString()}</p>
+            <br />
+            <p>Home Team: {currentGame[1]} <strong>{currentGame[2]}</strong></p>
+            <p>Away Team: {currentGame[3]} <strong>{currentGame[4]}</strong></p>
+            
+            <div style={{marginTop: 50}}>
+                <hr />
+                <p><strong>Game Thread</strong></p>
+                <hr />
+                {messages.map((message) => {
+                    return (
+                        <div className="message">
+                            <p>{message.time}</p>
+                            <p>{message.message}</p>
+                        </div>
+                    );
+                })}
+                <Search
+                    placeholder="Post a message about the game!"
+                    allowClear
+                    enterButton="Post"
+                    size="large"
+                    style={{marginTop: 50}}
+                />
+            </div>
+        </Modal>
         </div>
     );
 }
