9ad3561bac4a68a90046474fb2a50180c1b93a07
Thomas Fan <thomasfan100@gmail.com>
2021-10-30 10:23:07 -0500

    Pushing changes to title.

--- picklehacks/src/navbar/index.tsx
+++ picklehacks/src/navbar/index.tsx
@@ -7,6 +7,8 @@ function Navbar() {
   return (
     <div className="navbar">
       <img src={basketball} style={{marginLeft: 10, marginTop: 10, height: 50}} />
+      <p id = "nav_title">Pick-Le-Ball</p>
+      <img src={basketball} style={{marginLeft: 10, marginTop: 10, height: 50}} />
     </div>
   );
 }
